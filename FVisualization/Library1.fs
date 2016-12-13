61e4b690f0bc72d7592b06fa5c77426b8af29aab
sindresf <sindresfjermestad@gmail.com>
2016-12-13 12:39:55 +0100

    gather all the visualization stuff in one library.

---
+++ FVisualization/Library1.fs
@@ -0,0 +1,13 @@
+﻿namespace FVisualization
+
+type CA1D() = //iteratively make images with one more line added at a time for Gif "growing"
+    static member X = "F#"
+
+type CA2D() =  //whole board per image
+    static member X = "F#"
+
+type CPPNetwork() = //I dunno know about this... find a library for this
+    static member X = "F#"
+
+type PieChart() =
+    static member X = "F#"
